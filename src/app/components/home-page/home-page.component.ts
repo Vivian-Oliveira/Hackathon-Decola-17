c03d8d3ade05e082d7c269c94f85abc2ecdec9ab
Marilha Seixas <msc.marilha@gmail.com>
2021-06-19 17:39:54 +0100

    Inseri titulo na home-page

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -7,7 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() { }
+  titulo: string = '';
+  imagem: string = '';
+  texto: string = '';
+
+  public infoPost: HomePageComponent;
+
+  constructor() {
+    this.infoPost = new HomePageComponent();
+    this.infoPost.titulo = 'Primeira postagem!';
+    this.infoPost.imagem = 'kkkk';
+    this.infoPost.texto = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a tortor eu  odio pellentesque ullamcorper. Duis non ipsum mauris. Nullam dolor dui';
+   }
 
   ngOnInit(): void {
   }
