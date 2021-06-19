9e2d84d2bcf7035a39fc3866d2f7c6f759700c1a
Jose Sperandio <jose1234roberto@gmail.com>
2021-06-19 14:35:21 -0300

    style

--- src/app/components/help-page/help-page.component.ts
+++ src/app/components/help-page/help-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { InstituicoesMulheres } from 'src/app/services/instituicoes-mulheres';
 
 @Component({
   selector: 'app-help-page',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./help-page.component.css']
 })
 export class HelpPageComponent implements OnInit {
+
+
 
   constructor() { }
 
