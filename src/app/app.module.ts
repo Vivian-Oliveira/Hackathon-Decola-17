b930d5c6c3946faf430db847eaa4f3594501e298
Gabrielle Rossi <gabrielleluzrossi@gmail.com>
2021-06-19 14:29:38 -0300

    Chamada API instituicoes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { HttpClientModule } from '@angular/common/http';
+import { InstituicoesCardComponent } from './components/instituicoes-card/instituicoes-card.component';
 
 
 
@@ -28,7 +29,8 @@ import { HttpClientModule } from '@angular/common/http';
     HomePageComponent,
     HelpPageComponent,
     MenuTemplateComponent,
-    LawPageComponent
+    LawPageComponent,
+    InstituicoesCardComponent
   ],
   imports: [
     BrowserModule,
