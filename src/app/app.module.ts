84cb70915855b333f4a02d680df73317b6388850
Jose Sperandio <jose1234roberto@gmail.com>
2021-06-19 11:05:54 -0300

    services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { HelpPageComponent } from './components/help-page/help-page.component';
+import { MenuTemplateComponent } from './components/menu-template/menu-template.component';
+import { LawPageComponent } from './components/law-page/law-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    HelpPageComponent,
+    MenuTemplateComponent,
+    LawPageComponent
   ],
   imports: [
     BrowserModule,
