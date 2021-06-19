7c18d3359255267d699abbdc7ca32e5ffc6ca207
Jose Sperandio <jose1234roberto@gmail.com>
2021-06-19 15:30:09 -0300

    stylecss

--- src/app/components/instituicoes-card/instituicoes-card.component.ts
+++ src/app/components/instituicoes-card/instituicoes-card.component.ts
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-import { Component, OnInit } from '@angular/core';
-
-=======
 import { Component, OnInit, Input } from '@angular/core';
 import {InstituicoesMulheres} from 'src/app/services/instituicoes-mulheres';
->>>>>>> bd82c26949d9f9a6bed7263646e9b07b1755a582
 
 @Component({
   selector: 'app-instituicoes-card',
@@ -13,12 +8,9 @@ import {InstituicoesMulheres} from 'src/app/services/instituicoes-mulheres';
 })
 export class InstituicoesCardComponent implements OnInit {
 
-<<<<<<< HEAD
-=======
 
   @Input() instituicao: InstituicoesMulheres | undefined;
 
->>>>>>> bd82c26949d9f9a6bed7263646e9b07b1755a582
   constructor() { }
 
   ngOnInit(): void {
