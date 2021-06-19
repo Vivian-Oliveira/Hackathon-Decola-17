905252aebd44fc80e20e78b1c6798880b748b43b
Gabrielle Rossi <gabrielleluzrossi@gmail.com>
2021-06-19 17:16:41 -0300

    titulo e texto

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -9,7 +9,7 @@ export class HomePageComponent implements OnInit {
 
   public logoUrl = 'http://blog.donadivasemijoias.com.br/wp-content/uploads/2017/03/mulheres-felizes-realizadas-pessoal-e-financeiramente.jpg'
   public titulo = 'Mulheres Felizes Brilham Mais!!!!'
-  public descricao = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+  public descricao = 'O autocuidado é essencial para a nossa saúde mental e bem-estar, e ouvir o que o nosso corpo precisa faz com que nos tornemos fortes e mais felizes. Quando aceitamos nossos pontos fortes e fracos, quando nos aceitamos como somos, ganhamos mais auto confiança.'
 
   constructor() { }
 
