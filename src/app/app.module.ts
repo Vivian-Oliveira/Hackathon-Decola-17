f4f90ca5270259e5777497e9f166969948afd697
Gabrielle Rossi <gabrielleluzrossi@gmail.com>
2021-06-19 11:47:25 -0300

    menu-template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { HomePageComponent } from './components/home-page/home-page.component';
 import { HelpPageComponent } from './components/help-page/help-page.component';
 import { MenuTemplateComponent } from './components/menu-template/menu-template.component';
 import { LawPageComponent } from './components/law-page/law-page.component';
+import { MatCardModule } from '@angular/material/card';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { LawPageComponent } from './components/law-page/law-page.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
