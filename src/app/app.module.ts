fa4728cca6faa873a59ef9bd2fdabd86925129f8
Jose Sperandio <jose1234roberto@gmail.com>
2021-06-19 12:01:43 -0300

    AppModuleAddMatCard

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,6 +10,17 @@ import { MenuTemplateComponent } from './components/menu-template/menu-template.
 import { LawPageComponent } from './components/law-page/law-page.component';
 import { MatCardModule } from '@angular/material/card';
 
+
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+
+
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,7 +33,15 @@ import { MatCardModule } from '@angular/material/card';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatCardModule
+    MatCardModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
+
+
   ],
   providers: [],
   bootstrap: [AppComponent]
